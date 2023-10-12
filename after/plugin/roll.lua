4892397620f50b16e86e348c2641d30f06a7c5de
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2023-10-13 01:19:42 +0200

    Starting with default scheme and set the bg to light, so it resets any setting by the user

--- after/plugin/roll.lua
+++ after/plugin/roll.lua
@@ -2,10 +2,12 @@ if vim.g.loaded_roll then
   return
 end
 
-vim.g.loaded_roll = true
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
+vim.api.nvim_create_autocmd({ "UIEnter" }, {
   callback = function()
+    vim.opt.background = "light"
+    vim.cmd("colorscheme default")
+    vim.g.loaded_roll = true
     repeat
       require("roll").roll()
     until vim.opt.background._value == "dark"
